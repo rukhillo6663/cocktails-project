927668de708694c683ba3910d0549a1f2ade6041
Rukhillo Kodirov <rukhillo6663@gmail.com>
2023-03-16 15:03:58 +0900

    drink card JS done

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
 import { data } from './getData.js'
+const cards = document.querySelector('.cards')
 
 const drinksData = data();
-console.log(drinksData[0].strDrinkThumb, '<<<<this is my imported data')
+drinksData.forEach((card) => {
+
+  const div = document.createElement('div');
+  div.className = 'card';
+  cards.appendChild(div)
+  const image =document.createElement('img');
+  image.src = card.strDrinkThumb 
+  image.style.width = "250px"
+  div.appendChild(image)
+  const title = document.createElement('div');
+  title.className = 'title';
+  title.innerHTML = card.strDrink
+  div.appendChild(title)
+    
+    
+});
