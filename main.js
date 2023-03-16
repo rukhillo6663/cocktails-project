f3754d4d579e7ab02ba5e542a8c3d952f65b498f
Rukhillo Kodirov <rukhillo6663@gmail.com>
2023-03-16 16:04:04 +0900

    added extra code into main JS

--- main.js
+++ main.js
@@ -1,20 +1,34 @@
 import { data } from './getData.js'
 const cards = document.querySelector('.cards')
+const search = document.querySelector('input');
+
 
 const drinksData = data();
-drinksData.forEach((card) => {
+const result =drinksData.forEach((card) => {
 
   const div = document.createElement('div');
   div.className = 'card';
   cards.appendChild(div)
   const image =document.createElement('img');
   image.src = card.strDrinkThumb 
-  image.style.width = "250px"
+  image.style.width = "100%";
+  image.style.height = '100%';
   div.appendChild(image)
   const title = document.createElement('div');
   title.className = 'title';
   title.innerHTML = card.strDrink
-  div.appendChild(title)
+  div.appendChild(title);
     
+})
+
+
+search.addEventListener("keyup", (event)=>{
+    drinksData.forEach((item) =>{
+        const value = search.value.toLowerCase();
+        const compareValue = (item.strDrink).toLowerCase();
+        if(compareValue.includes(value)){
+            //console.log(value)
+        }
+    })
+})
     
-});
