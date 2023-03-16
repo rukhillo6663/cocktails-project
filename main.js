d7e912a2cd5719f44f4493667ee52ca722367157
Rukhillo Kodirov <rukhillo6663@gmail.com>
2023-03-16 14:19:44 +0900

    card HTML and CSS added

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 import { data } from './getData.js'
 
 const drinksData = data();
-console.log(drinksData, '<<<<this is my imported data')
+console.log(drinksData[0].strDrinkThumb, '<<<<this is my imported data')
